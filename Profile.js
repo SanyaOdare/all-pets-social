f5f8595434949b89b083a5f415bb0ab0e8648b0a
Sanya Oluwadare <sanyaodare@gmail.com>
2021-07-19 18:32:38 +0100

    unmount component

--- Profile.js
+++ Profile.js
@@ -9,13 +9,33 @@ export class Profile extends React.Component {
   }
 
   loadUserData() {
+    this.setState = ({ userData: null });
     this.fetchID = fetchUserData(this.props.username, (userData) => {
       this.setState({ userData });
     });
   }
 
+  componentDidMount() {
+    this.loadUserData();
+  }
+
+  componentWillUnmount() {
+    cancelFetch(this.fetchID);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.username !== prevProps.username) {
+      cancelFetch(this.fetchID);
+      this.load.loadUserData();
+    }
+  }
+
   render() {
-    const isLoading = true;
+    const isLoading = this.state.userData === null ? true : false;
+    
+    const name = isLoading ? 'Loading...' : this.state.userData.name;
+    const bio = isLoading ? 'Bio goes here' : this.state.userData.bio;
+    const friends = isLoading ? [] : this.state.userData.friends;
 
     let className = 'Profile';
     if (isLoading) {
@@ -24,11 +44,17 @@ export class Profile extends React.Component {
 
     return (
       <div className={className}>
-        <div className="profile-picture"></div>
+        <div className="profile-picture">
+          {
+            isLoading === false
+            &&
+            <img src={this.state.userData.profilePictureUrl} alt="" />
+          }
+        </div>
         <div className="profile-body">
-          <h2>Name goes here</h2>
-          <h3>@{this.props.username}</h3>
-          <p>Bio goes here</p>
+          <h2>{ name }</h2>
+          <h3>@{ this.props.username }</h3>
+          <p>{ bio }</p>
           <h3>My friends</h3>
           <Userlist usernames={[]} onChoose={this.props.onChoose} />
         </div>
